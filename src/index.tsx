b0d8cf5368fdedd18dc31af04f4d486fcbc9f44b
Cloudbop <boppincolin@gmail.com>
2021-08-10 18:27:44 +0100

    bp component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hi</div>;
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const onClick = (): void => {
+    console.log(`input`, input);
+  };
+
+  return (
+    <div>
+      <textarea
+        name=""
+        id=""
+        value={input}
+        onChange={(e): void => setInput(e.target.value)}
+        // htmlCols="30" htmlRows="10"
+      ></textarea>
+      <div>
+        <button
+          onClick={() => {
+            onClick();
+            setCode(input);
+          }}
+        >
+          Transpile
+        </button>
+      </div>
+
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
