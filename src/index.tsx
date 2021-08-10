754f61272b3700e3f86d1aa4ff1b140e5a725cb8
Cloudbop <boppincolin@gmail.com>
2021-08-10 18:37:37 +0100

    implmntd ebsuild wasm API! awesome-o

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,32 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import * as esbuild from "esbuild-wasm";
 import ReactDOM from "react-dom";
 
 const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      // our binary lives here
+      wasmURL: "/esbuild.wasm",
+    });
+    // access wasm API
+    console.log(service);
+  };
+
   const onClick = (): void => {
     console.log(`input`, input);
   };
+
+  useEffect(() => {
+    //
+    startService();
+    //
+    // return () => {
+    // }
+  }, []);
 
   return (
     <div>
