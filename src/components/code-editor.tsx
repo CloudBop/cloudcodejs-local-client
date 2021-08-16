665127dec77a40fca69773ea5900fe0a987df58f
Cloudbop <boppincolin@gmail.com>
2021-08-16 23:03:21 +0100

    +prettier+format implmntation

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -2,6 +2,9 @@ import MonacoEditor, {
   // type definition
   EditorDidMount,
 } from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
+import { useRef } from "react";
 
 interface CodeEditorProps {
   initialValue: string;
@@ -9,6 +12,7 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<any>();
   // using typedef to annotate arity
   const onEditorDidMount: EditorDidMount = (
     // current string value editor is set to
@@ -16,8 +20,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
     // monacoEditorRef
     monacoEditor
   ) => {
-    // onchange
-
+    editorRef.current = monacoEditor;
     monacoEditor.onDidChangeModelContent(() => {
       // console.log(getValue());
       onChange(getValue());
@@ -25,31 +28,48 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
     // set tab size
     monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
   };
+  const onFormatClick = () => {
+    //get current value from editor
+    const unformatted = editorRef.current.getModel().getValue();
+    // format
+    const formatted = prettier.format(unformatted, {
+      parser: "babel",
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+    // set formatted value
+    editorRef.current.setValue(formatted);
+  };
   return (
-    <MonacoEditor
-      editorDidMount={onEditorDidMount}
-      value={initialValue} //-really an initial value
-      // see monaco type defs - beware, its huge
-      options={{
-        wordWrap: "on",
-        minimap: { enabled: false },
-        // don't fade unused
-        showUnused: false,
-        // collapse right hand side of line numbers
-        folding: false,
-        //collapse left hand side of line numbers
-        lineNumbersMinChars: 3,
-        //
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        // redraw layout changes
-        automaticLayout: true,
-      }}
-      // includes syntax-highlight, intellisense ect
-      language={"javascript"}
-      theme={"dark"}
-      height={"500px"}
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        editorDidMount={onEditorDidMount}
+        value={initialValue} //-really an initial value
+        // see monaco type defs - beware, its huge
+        options={{
+          wordWrap: "on",
+          minimap: { enabled: false },
+          // don't fade unused
+          showUnused: false,
+          // collapse right hand side of line numbers
+          folding: false,
+          //collapse left hand side of line numbers
+          lineNumbersMinChars: 3,
+          //
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          // redraw layout changes
+          automaticLayout: true,
+        }}
+        // includes syntax-highlight, intellisense ect
+        language={"javascript"}
+        theme={"dark"}
+        height={"500px"}
+      />
+    </div>
   );
 };
 
