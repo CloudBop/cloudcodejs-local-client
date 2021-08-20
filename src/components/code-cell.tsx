f7db905234083e1b3ca1e5aa20a06718675da772
Cloudbop <boppincolin@gmail.com>
2021-08-20 16:46:50 +0100

    Refactored index.tsx into <CodeCell/>

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import bundle from "../bundler";
+import CodeEditor from "./code-editor";
+import Preview from "./code-preview";
+const CodeCell = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const onClick = async () => {
+    const output = await bundle(input);
+    setCode(output);
+  };
+
+  return (
+    <div>
+      <CodeEditor
+        initialValue={"const a = 1;"}
+        onChange={(value): void => setInput(value)}
+      />
+      <div>
+        <button
+          onClick={() => {
+            onClick();
+            // setCode(input);
+          }}
+        >
+          Transpile
+        </button>
+        <pre>{code}</pre>
+      </div>
+      <Preview code={code} />
+    </div>
+  );
+};
+
+export default CodeCell;
--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,12 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import { useState } from "react";
-import bundle from "./bundler";
 import ReactDOM from "react-dom";
-import CodeEditor from "./components/code-editor";
-import Preview from "./components/code-preview";
+import CodeCell from "./components/code-cell";
+
 const App = () => {
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue={"const a = 1;"}
-        onChange={(value): void => setInput(value)}
-      />
-      <div>
-        <button
-          onClick={() => {
-            onClick();
-            // setCode(input);
-          }}
-        >
-          Transpile
-        </button>
-        <pre>{code}</pre>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 };
