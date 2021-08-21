b67faf4807542f46e3919718940abb0cdebdc85d
Cloudbop <boppincolin@gmail.com>
2021-08-22 00:04:42 +0100

    update+execute code on keychange, via debounce

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import bundle from "../bundler";
 import CodeEditor from "./code-editor";
 import Preview from "./code-preview";
@@ -7,10 +7,22 @@ const CodeCell = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  // const onClick = async () => {
+  //   const output = await bundle(input);
+  //   setCode(output);
+  // };
+
+  useEffect(() => {
+    let timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 1000);
+
+    return () => {
+      // offMount|rerender
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   return (
     <Resizable direction="vertical">
