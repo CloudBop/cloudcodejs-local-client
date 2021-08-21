52816330e335d8f7d03b8c2d7eafadacbef3870d
Cloudbop <boppincolin@gmail.com>
2021-08-21 18:18:27 +0100

    reset initialisers on rerenders- breaks min/max constraints

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -10,6 +10,10 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
   const [innerHeight, setInnerHeight] = useState(window.innerHeight);
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  // initialiser state, needed for re-renders || width/height values won't be updated on change
+  const [width, setWidth] = useState(window.innerWidth * 0.75);
+
   useEffect(() => {
     let timer: any;
     const listen = () => {
@@ -38,8 +42,12 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
       maxConstraints: [innerWidth * 0.9, Infinity],
       // cheat the 100% problem
       height: Infinity,
-      width: innerWidth * 0.75,
+      width: width,
       resizeHandles: ["e"],
+      onResizeStop: (evt, data) => {
+        //onDragEnd
+        setWidth(data.size.width);
+      },
     };
   } else {
     resizableProps = {
