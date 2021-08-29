926298106fe442eecd1dd827eea3a2a544b7c093
Cloudbop <boppincolin@gmail.com>
2021-08-29 22:43:23 +0100

    +action-creators  stubs with return type annotations

--- src/reduxStore/action-creators/index.ts
+++ src/reduxStore/action-creators/index.ts
@@ -1,4 +1,12 @@
-export const updateCell = () => ({});
-export const deleteCell = () => {};
-export const insertCellBefore = () => {};
-export const moveCell = () => {};
+import { ActionType } from "../action-types";
+import {
+  DeleteCellAction,
+  InsertCellBeforeAction,
+  MoveCellAction,
+  UpdateCellAction,
+} from "../actions";
+
+export const updateCell = (): UpdateCellAction => ({});
+export const deleteCell = (): DeleteCellAction => ({});
+export const insertCellBefore = (): InsertCellBeforeAction => ({});
+export const moveCell = (): MoveCellAction => ({});
--- src/reduxStore/actions/index.ts
+++ src/reduxStore/actions/index.ts
@@ -1,7 +1,7 @@
 import { ActionType } from "../action-types";
 import { CellTypes } from "../cell";
 
-interface MoveCellAction {
+export interface MoveCellAction {
   type: ActionType.MOVE_CELL;
   payload: {
     id: string;
@@ -9,18 +9,18 @@ interface MoveCellAction {
   };
 }
 
-interface DeleteCellAction {
+export interface DeleteCellAction {
   type: ActionType.DELETE_CELL;
   payload: string;
 }
-interface InsertCellBeforeAction {
+export interface InsertCellBeforeAction {
   type: ActionType.INSERT_CELL_BEFORE;
   payload: {
     id: string;
     type: CellTypes;
   };
 }
-interface UpdateCellAction {
+export interface UpdateCellAction {
   type: ActionType.UPDATE_CELL;
   payload: {
     id: string;
@@ -28,6 +28,7 @@ interface UpdateCellAction {
   };
 }
 
+// union type
 export type Action =
   | MoveCellAction
   | DeleteCellAction
