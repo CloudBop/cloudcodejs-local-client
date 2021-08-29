153e568c4c0d650389acc8ae5270937e0ee1eb13
Cloudbop <boppincolin@gmail.com>
2021-08-29 22:36:36 +0100

    connexted reduxStore to JSX via +Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./reduxStore";
 import CodeCell from "./components/code-cell";
 import TextEditor from "./components/text-editor";
 
 const App = () => {
   return (
-    <div
-    // className={"app"}
-    // style={{ height: "100%", display: "flex", flexDirection: "row" }}
-    >
-      {/* <CodeCell /> */}
-      {/* <CodeCell /> */}
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div
+      // className={"app"}
+      // style={{ height: "100%", display: "flex", flexDirection: "row" }}
+      >
+        {/* <CodeCell /> */}
+        {/* <CodeCell /> */}
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
