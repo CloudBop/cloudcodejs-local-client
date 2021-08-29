0c17c19992fe173575beda515038c9ae71396a1c
Cloudbop <boppincolin@gmail.com>
2021-08-29 23:00:53 +0100

    +action-creators +type Direction

--- src/reduxStore/action-creators/index.ts
+++ src/reduxStore/action-creators/index.ts
@@ -1,12 +1,39 @@
+import { CellTypes } from "../cell";
 import { ActionType } from "../action-types";
 import {
   DeleteCellAction,
   InsertCellBeforeAction,
   MoveCellAction,
   UpdateCellAction,
+  Direction,
 } from "../actions";
 
-export const updateCell = (): UpdateCellAction => ({});
-export const deleteCell = (): DeleteCellAction => ({});
-export const insertCellBefore = (): InsertCellBeforeAction => ({});
-export const moveCell = (): MoveCellAction => ({});
+export const updateCell = (id: string, content: string): UpdateCellAction => ({
+  type: ActionType.UPDATE_CELL,
+  payload: {
+    id,
+    content,
+  },
+});
+export const deleteCell = (id: string): DeleteCellAction => ({
+  type: ActionType.DELETE_CELL,
+  payload: id,
+});
+export const insertCellBefore = (
+  id: string,
+  cellType: CellTypes
+): InsertCellBeforeAction => ({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id,
+    type: cellType,
+  },
+});
+
+export const moveCell = (id: string, direction: Direction): MoveCellAction => ({
+  type: ActionType.MOVE_CELL,
+  payload: {
+    id,
+    direction,
+  },
+});
