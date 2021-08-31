1cc679bc97230a5fed5ec1d7445d2d93d5ae4bc6
Cloudbop <boppincolin@gmail.com>
2021-08-31 01:03:47 +0100

    +useTypedSelector

---
+++ src/hooks/use-typed-selector.tsx
@@ -0,0 +1,3 @@
+import { useSelector, TypedUseSelectorHook } from "react-redux";
+import { RootState } from "../reduxStore";
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
