bdf5d20037ec210c844762911e087bb1a4a1da2e
Cloudbop <boppincolin@gmail.com>
2021-08-31 22:26:31 +0100

    +memoised bindActionCreators , now can use as useEffect dep arr cnextdiff

--- src/hooks/use-bound-action.ts
+++ src/hooks/use-bound-action.ts
@@ -1,7 +1,11 @@
+import { useMemo } from "react";
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../reduxStore";
 export const useBoundActions = () => {
   const dispatch = useDispatch();
-  return bindActionCreators(actionCreators, dispatch);
+  return useMemo(() => bindActionCreators(actionCreators, dispatch), [
+    dispatch,
+  ]);
+  // return bindActionCreators(actionCreators, dispatch);
 };
