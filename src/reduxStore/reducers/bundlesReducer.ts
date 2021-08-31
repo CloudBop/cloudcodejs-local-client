a8936b8c4846f5446a17036d8926cfa2f055398a
Cloudbop <boppincolin@gmail.com>
2021-08-31 21:27:12 +0100

    +bp bundlers reducer

--- src/reduxStore/reducers/bundlesReducer.ts
+++ src/reduxStore/reducers/bundlesReducer.ts
@@ -1,1 +1,19 @@
-export default 1;
+import produce from "immer";
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+interface BundleState {
+  [key: string]: {
+    loading: boolean;
+    code: string;
+    err: string;
+  };
+}
+
+const initialState: BundleState = {};
+
+const bundleReducer = produce(
+  (state: BundleState = initialState, action: Action): BundleState => {
+    return state;
+  }
+);
