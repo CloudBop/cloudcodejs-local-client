d3feff148842b3dc5e9251774c8ab422c6f81df4
Cloudbop <boppincolin@gmail.com>
2021-08-31 13:17:23 +0100

    +.action-bar-wrapper +styling

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -2,22 +2,31 @@ import { Cell } from "../reduxStore";
 import CellActionBar from "./cell-action-bar";
 import CodeCell from "./code-cell";
 import TextEditor from "./text-editor";
-
+import "./cell-list-item.css";
 interface CellListItemProps {
   cell: Cell;
 }
 
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
-  if (cell.type === "code") child = <CodeCell cell={cell} />;
-  else child = <TextEditor cell={cell} />;
+  if (cell.type === "code")
+    child = (
+      <>
+        <div className="action-bar-wrapper">
+          <CellActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    );
+  else
+    child = (
+      <>
+        <TextEditor cell={cell} />
+        <CellActionBar id={cell.id} />
+      </>
+    );
 
-  return (
-    <div className="cell-list-item">
-      {child}
-      <CellActionBar id={cell.id} />
-    </div>
-  );
+  return <div className="cell-list-item">{child}</div>;
 };
 
 export default CellListItem;
