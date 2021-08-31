a00087df2535209732e18388e936f0afafeaf261
Cloudbop <boppincolin@gmail.com>
2021-09-01 00:42:28 +0100

    +JSX render if React imported

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -22,11 +22,18 @@ const CodeCell: React.FC<ICodeCellProps> = ({ cell }) => {
       // global
       `
         const show = (value)=>{
+          const root = document.querySelector('#root')
 
           if(typeof value === "object"){
-            document.querySelector('#root').innerHTML = JSON.stringify(value)
+
+            if(value.$$typeof&& value.props){
+              ReactDOM.render(value, root)
+            } else {
+              root.innerHTML = JSON.stringify(value)
+            }
+
           } else{
-            document.querySelector('#root').innerHTML = value;
+            root.innerHTML = value;
           }
         }
       `,
