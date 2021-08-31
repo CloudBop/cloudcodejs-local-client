a166b04d691d0c8d8bd81fad88cc0731b3a6fc08
Cloudbop <boppincolin@gmail.com>
2021-08-31 13:38:41 +0100

    styled add-cell +buttons

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -9,12 +9,26 @@ const AddCell: React.FC<IAddCellProps> = ({ nextCellId }) => {
   const { insertCellBefore } = useBoundActions();
   return (
     <div className="add-cell">
-      <button onClick={() => insertCellBefore(nextCellId, "code")}>
-        Add Code
-      </button>
-      <button onClick={() => insertCellBefore(nextCellId, "text")}>
-        Add MD
-      </button>
+      <div className="add-btns">
+        <button
+          className="button is-rounded is-primary is-small"
+          onClick={() => insertCellBefore(nextCellId, "code")}
+        >
+          <span className="icon is-smaall">
+            <i className="fas fa-plus"></i>
+          </span>
+          <span>Add Code</span>
+        </button>
+        <button
+          className="button is-rounded is-primary is-small"
+          onClick={() => insertCellBefore(nextCellId, "text")}
+        >
+          <span className="icon is-smaall">
+            <i className="fas fa-plus"></i>
+          </span>
+          <span>Add Markdown</span>
+        </button>
+      </div>
       <div className="divider"></div>
     </div>
   );
