ad6e5e5e5b26328a61e3fc7b63ae8a4be79b8760
Cloudbop <boppincolin@gmail.com>
2021-08-31 01:05:38 +0100

    +implmntd typeSelector

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,4 +1,7 @@
+import { useTypedSelector } from "../hooks/use-typed-selector";
 const CellList: React.FC = () => {
+  useTypedSelector((state) => state);
+  // useTypedSelector(({ cells: { order, data } }) => {});
   return <div>CellList</div>;
 };
 
