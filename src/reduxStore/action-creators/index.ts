38831d3f11fe85041feab82a715be755543d2016
Cloudbop <boppincolin@gmail.com>
2021-09-01 16:21:57 +0100

    +async actionCreator fetchCells()

--- src/reduxStore/action-creators/index.ts
+++ src/reduxStore/action-creators/index.ts
@@ -1,6 +1,7 @@
+import axios from "axios";
 //todo - refactor into many files
 import { Dispatch } from "react";
-import { CellTypes } from "../cell";
+import { CellTypes, Cell } from "../cell";
 import { ActionType } from "../action-types";
 import {
   DeleteCellAction,
@@ -68,5 +69,20 @@ export const createBundle = (cellId: string, inputRawCode: string) => {
         // },
       },
     });
+  };
+};
+
+export const fetchCells = () => {
+  return async (dispatch: Dispatch<Action>) => {
+    // flip loading state
+    dispatch({ type: ActionType.FETCH_CELLS });
+
+    try {
+      const { data }: { data: Cell[] } = await axios.get("/cells");
+
+      dispatch({ type: ActionType.FETCH_CELLS_COMPLETE, payload: data });
+    } catch (err) {
+      dispatch({ type: ActionType.FETCH_CELLS_ERROR, payload: err.message });
+    }
   };
 };
