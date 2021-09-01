981440bde06b87be0ac95017a659033769f112c1
Cloudbop <boppincolin@gmail.com>
2021-09-01 23:19:05 +0100

    +fetchCell bound action implementation

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,5 +1,6 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
+import { useBoundActions } from "../hooks/use-bound-action";
 import AddCell from "./add-cell";
 import CellListItem from "./cell-list-item";
 import "./cell-list.css";
@@ -8,6 +9,14 @@ const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
     order.map((id) => data[id])
   );
+
+  const { fetchCells } = useBoundActions();
+  useEffect(() => {
+    fetchCells();
+    // return () => {
+    //   cleanup
+    // }
+  }, [fetchCells]);
 
   const renderCells = cells.map((cell) => (
     <Fragment key={cell.id}>
